384c5a09598240719c44a2ae3e5d4e25ace461a8
IuliaOlteanu <73997287+IuliaOlteanu@users.noreply.github.com>
2024-08-13 15:33:48 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,16 +1,17 @@
-#include <string.h >
-#define goodPass ”GOODPASS”
+#include <string.h>
+#include <stdio.h>
+#define goodPass "GOODPASS"
 
 int main()
 {
     char passIsGood = 0;
     char buf[80];
 
-    printf(“Enter password : \n”);
+    printf("Enter password : \n");
     gets(buf);
 
     if (strcmp(buf, goodPass) == 0)
         passIsGood = 1;
     if (passIsGood == 1)
-        printf( ”You win ! \ n”);
+        printf("You win ! \n");
 }
