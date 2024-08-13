cd337db68c7de908640282d9e85abb45b22b040e
IuliaOlteanu <73997287+IuliaOlteanu@users.noreply.github.com>
2024-08-13 15:21:57 +0300

    Update object_attack.c

--- V1/3/object_attack.c
+++ V1/3/object_attack.c
@@ -3,7 +3,7 @@
 int main () {
     /* call hidden function from object.o with the "key1 key2" argument */
     
-    order_please("johns pizzeria", 4027431614);
+    order_please("key1 key2", argument);
     return 0;
 }
 0804939f
